7031a43e1705474e1c6949dc3781b5d1f902e287
Sergey Lebedev <lebedevss88@gmail.com>
2022-08-18 02:28:25 +0300

    Homework number 4 - done and added.

---
+++ HomeWork/HM4/Task27/Program.cs
@@ -0,0 +1,33 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+// using static System.Console;
+// // Задача 27
+// Clear();
+// WriteLine("Введите число cумму цифр которого  вы хотите узнать: ");
+// int number = Convert.ToInt32(ReadLine());
+// int DigitsSum = 0;
+// for (int i = 0; i < number; i++) // можно было использовать while number > 0 {.....}
+// {
+//     DigitsSum += number % 10;
+//     number = number / 10;
+// }
+// Console.WriteLine($"Cумма цифр в  введённом числе {DigitsSum}");
+
+// Напишите программу, которая принимает на вход число и выдаёт произведение цифр в числе.
+// 1234 --> 24
+// 6524 --> 240
+using static System.Console;
+// Задача самостоятельная
+Clear();
+WriteLine("Введите число произведение цифр которого вы хотите узнать: ");
+int number = Convert.ToInt32(ReadLine());
+int DigitsMultiplication = 1;
+for (int i = 0; i < number; i++) // можно было использовать while number != 0 {.....}
+{
+    DigitsMultiplication *= number % 10;
+    number = number / 10;
+}
+Console.WriteLine($"Произведение цифр в введённом числе {DigitsMultiplication}");
---
+++ HomeWork/HM4/Task29/Program.cs
@@ -0,0 +1,64 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+using static System.Console;
+// Задача 29
+Clear();
+
+void FillArray(int[] MyArray)
+{
+    int length = MyArray.Length;
+    int index = 0;
+    while (index < length)
+    {
+        MyArray[index] = new Random().Next(1, 100);
+        // MyArray[index] = int.Parse(ReadLine()!);  // -> Ввод с клавиатуры
+        index++;
+    }
+}
+
+void SortArrayMinToMax(int[] AnyArray)
+{
+    for (int i = 0; i < AnyArray.Length; i++)
+        for (int j = i + 1; j < AnyArray.Length; j++)
+            if (AnyArray[i] > AnyArray[j])
+            {
+                int temp = AnyArray[i];
+                AnyArray[i] = AnyArray[j];
+                AnyArray[j] = temp;
+            }
+}
+
+void SortArrayMaxToMin(int[] AnyArray)
+{
+    for (int i = 0; i < AnyArray.Length - 1; i++)
+        for (int j = i + 1; j < AnyArray.Length; j++)
+            if (AnyArray[i] < AnyArray[j])
+            {
+                int temp = AnyArray[i];
+                AnyArray[i] = AnyArray[j];
+                AnyArray[j] = temp;
+            }
+}
+
+void PrintArray(int[] AnyArray)
+{
+    int length = AnyArray.Length;
+    int index = 0;
+    while (index < length)
+    {
+        Write($"{AnyArray[index]} ");
+        index++;
+    }
+}
+
+int[] array = new int[8];
+FillArray(array);
+PrintArray(array);
+WriteLine();
+SortArrayMinToMax(array);
+PrintArray(array);
+WriteLine();
+SortArrayMaxToMin(array);
+PrintArray(array);
