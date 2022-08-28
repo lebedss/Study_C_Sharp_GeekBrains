ee3d87eaee9bee3b99fb68c0ee86748753e93d4a
Sergey Lebedev <lebedevss88@gmail.com>
2022-08-28 03:10:13 +0300

    minor bug fixed

--- Seminars/Seminar-6/Task6/Program.cs
+++ Seminars/Seminar-6/Task6/Program.cs
@@ -7,5 +7,5 @@ using System.Linq;
 Clear();
 
 WriteLine("Введите массив через пробел: ");
-int count = ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(x => int.Parse(x)).ToArray().Where(a => a % 2 != 0).Count();
+int count = ReadLine()!.Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(x => int.Parse(x)).ToArray().Where(a => a % 2 != 0).Count();
 WriteLine($"Количество нечётных элементов в этом массиве - {count}");
