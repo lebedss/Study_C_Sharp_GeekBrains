01692f842dcd4aa6ea73f9790e97acf89666de62
Sergey Lebedev <lebedevss88@gmail.com>
2022-09-04 20:07:33 +0300

    work in progress

---
+++ HomeWork/HW8/Task54/Program.cs
@@ -0,0 +1,69 @@
+﻿// Задача 54: Задайте двумерный массив. 
+// Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// В итоге получается вот такой массив:
+// 7 4 2 1
+// 9 5 3 2
+// 8 4 4 2
+
+
+using static System.Console;
+using System.Linq;
+Clear();
+
+Write("Введите размеры массива(количество строк и столбцов), мин и макс значения через пробел: ");
+int[] parameters = ReadLine()!.Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(x => int.Parse(x)).ToArray();
+int[,] matrix = GetMatrixArray(parameters[0], parameters[1], parameters[2], parameters[3]);
+PrintMatrixArray(matrix);
+WriteLine();
+SortStringsInMatrixMinToMax(matrix);
+PrintMatrixArray(matrix);
+
+
+int[,] GetMatrixArray(int rows, int columns, int minValue, int maxValue)
+{
+    int[,] result = new int[rows, columns];
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            result[i, j] = new Random().Next(minValue, maxValue + 1);
+        }
+    }
+    return result;
+}
+
+
+void PrintMatrixArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Write($"{inArray[i, j]} ");
+        }
+        WriteLine();
+    }
+}
+
+void SortStringsInMatrixMinToMax(int[,] anyArray)
+{
+    for (int i = 0; i < anyArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < anyArray.GetLength(1); j++)
+        {
+            for (int k = 0; k < anyArray.GetLength(1) - 1; k++)
+            {
+                if (anyArray[i, j] <= anyArray[i, k]) continue;
+                {
+                    int temp = anyArray[i, j];
+                    anyArray[i, j] = anyArray[i, k];
+                    anyArray[i, k] = temp;
+                }
+            }
+        }
+    }
+}
---
+++ HomeWork/HW8/Task56/Program.cs
@@ -0,0 +1,100 @@
+﻿// Задача 56: 
+// Задайте прямоугольный двумерный массив. 
+// Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 5 2 6 7
+// Программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей суммой элементов: 1 строка
+
+
+using static System.Console;
+using System.Linq;
+Clear();
+
+Write("Введите  целое число - N, чтобы получить прямоугольный двумерный массив N x N: ");
+int[] parameters = ReadLine()!.Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(x => int.Parse(x)).ToArray();
+int[,] matrix = GetMatrixArray(parameters[0], parameters[0]);
+PrintMatrixArray(matrix);
+// GetMinSumOfMatrixRaw(matrix);
+WriteLine($"Номер строки с наименьшей суммой элементов: {GetTheMatrixLineWithMinSum(matrix)}");
+
+
+// void GetMinSumOfMatrixRaw(int[,] anyArray)
+// {
+//     int minSum = Int32.MaxValue; //  Int32.MaxValue это константа - крайнее значение, которое может принять тип int : 2147483647
+//     int linePosition = 0;
+//     for (int i = 0; i < anyArray.GetLength(0); i++)
+//     {
+//         int sum = 0;
+//         for (int j = 0; j < anyArray.GetLength(1); j++)
+//         {
+//             sum += anyArray[i, j];
+//         }
+//         if (sum < minSum)
+//         {
+//             minSum = sum;
+//             linePosition++;
+//         }
+//     }
+//     WriteLine($"Строка с наименьшей суммой элементов: {linePosition}, а сумма элементов в этой строке: {minSum}");
+// }
+
+
+int[,] GetMatrixArray(int rows, int columns)
+{
+    int[,] result = new int[rows, columns];
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            result[i, j] = new Random().Next(1, 10);
+        }
+    }
+    return result;
+}
+
+
+void PrintMatrixArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Write($"{inArray[i, j]} ");
+        }
+        WriteLine();
+    }
+}
+
+
+int MatrixLineElementsSum(int[,] anyArray, int linePosition)
+{
+    int sumLine = anyArray[linePosition, 0];
+    for (int j = 1; j < anyArray.GetLength(1); j++)
+    {
+        sumLine += anyArray[linePosition, j];
+    }
+    return sumLine;
+}
+
+
+int GetTheMatrixLineWithMinSum(int[,] myArray)
+{
+    int minLinesum = 0;
+    int sumInline = MatrixLineElementsSum(myArray, 0);
+    for (int i = 1; i < myArray.GetLength(0); i++)
+    {
+        int tempLineSum = MatrixLineElementsSum(myArray, i);
+        if (sumInline > tempLineSum)
+        {
+            sumInline = tempLineSum;
+            minLinesum = i;
+        }
+    }
+    return minLinesum;
+}
+
+
+
